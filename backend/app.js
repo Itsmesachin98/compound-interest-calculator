3fc724de9ca5eae9c2dfde5f4131a668f8c90bd2
Sachin Kumar <sachinkumarskpog@gmail.com>
2025-08-29 13:19:31 +0530

    chore: configure app to use process.env.PORT

--- backend/app.js
+++ backend/app.js
@@ -97,4 +97,5 @@ function calculateWithDeposits(principal, rate, time, deposits, choice) {
     };
 }
 
-app.listen(3000, () => console.log(`LISTENING ON PORT 3000`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
